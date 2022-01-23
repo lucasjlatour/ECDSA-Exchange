496aa123ad35a88160888e7c4df81eef14aebb73
Lucas La Tour <lucasjlatour@gmail.com>
2022-01-23 18:00:17 -0500

    added public keys

--- server/index.js
+++ server/index.js
@@ -2,17 +2,23 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const port = 3042;
+const EC = require('elliptic').ec;
+const ec = new EC('secp256k1');
+
+// generate 3 new keys
+const key1 = ec.genKeyPair();
+const key2 = ec.genKeyPair();
+const key3 = ec.genKeyPair();
 
 // localhost can have cross origin errors
 // depending on the browser you use!
 app.use(cors());
 app.use(express.json());
 
-const balances = {
-  "1": 100,
-  "2": 50,
-  "3": 75,
-}
+const balances = {};
+balances[key1.getPublic().encode('hex')] = 100;
+balances[key2.getPublic().encode('hex')] = 50;
+balances[key3.getPublic().encode('hex')] = 75;
 
 app.get('/balance/:address', (req, res) => {
   const {address} = req.params;
@@ -29,4 +35,5 @@ app.post('/send', (req, res) => {
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}!`);
+  console.log(balances);
 });
